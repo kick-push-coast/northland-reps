f7563c14dc1f8e314cc815c27512c6e86e141524
michaeltyler <mktyler01@gmail.com>
2017-04-30 01:44:23 -0700

    Majority development finished. Design review.

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -16,6 +16,9 @@
       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+
+      <link rel="icon" href="images/icon.png">
 
       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
       <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -27,14 +30,14 @@
    <body>
       <div class="treeblock">
       <div class="header" id="header">
-         <img class="headerlogo" src="images/headerlogo.svg"/>
+         <img class="headerlogo" id="headerlogo" src="images/headerlogo.svg"/>
          <div class="headerinfo">
             <div class="headercontact">
-               <p>Phone: &#40;952&#41; 854-3905</p>
-               <p>Fax: &#40;952&#41; 854-5210</p>
+               <p><a href="tel:952-854-3905">Phone: &#40;952&#41; 854-3905</a></p>
+               <p><a href="fax:952-854-5210">Fax: &#40;952&#41; 854-5210</a></p>
             </div>
             <div class="headeraddress">
-               <p>500 Third St. SE</br>Osseo, MN 55369</p>
+               <p><a href="https://www.google.com/maps/place/500+3rd+St+SE,+Osseo,+MN+55369/@45.1122785,-93.397816,17z/data=!3m1!4b1!4m5!3m4!1s0x52b339cc63dfc2f3:0x3570656943a6eef5!8m2!3d45.1122747!4d-93.3956273">500 Third St. SE</br>Osseo, MN 55369</a></p>
             </div>
          </div>
       </div>
@@ -51,18 +54,44 @@
       </div>
       <div class="intro" id="intro">
          <div class="introleft">
-            <h1>A History of Successful Representation</h1>
-            <p>
-               Northland Reps was established in the fall of 1992 when the name of Douglas Greig &amp; Associates Company, established in 1960 was changed. Today we still represent companies that have been with us for more than 30 years. Twenty years from now, history will show that Northland Reps continued to represent their manufacturers with knowledge and integrity; these being two major ingredients in any successful rep agency.
-            </p>
-            <h1>Our Mission</h1>
-            <p>
+            <div>
+               <h1>A History of Successful Representation</h1>
+               <p>
+                  Northland Reps was established in the fall of 1992 when the name of Douglas Greig &amp; Associates Company, established in 1960 was changed. Today we still represent companies that have been with us for more than 30 years. Twenty years from now, history will show that Northland Reps continued to represent their manufacturers with knowledge and integrity; these being two major ingredients in any successful rep agency.
+               </p>
+            </div>
+            <div>
+               <h1>Our Mission</h1>
+               <p>
 
-               Northland Reps and the manufacturers they represent have one goal in mind; to offer you quality products at a fair price delivered in a timely manner. To accomplish our mission, we strive to provide our customer's the service and support that is essential for guaranteed customer satisfaction.
-            </p>
+                  Northland Reps and the manufacturers they represent have one goal in mind: to offer you quality products at a fair price delivered in a timely manner. To accomplish our mission, we strive to provide our customer's the service and support that is essential for guaranteed customer satisfaction.
+               </p>
+            </div>
          </div>
-         <div class="introright">
+         <!-- <div class="introright">
             <h1>We get your product to the people that need it.</h1>
+         </div> -->
+         <div class="introrightnew">
+            <div class="introphotoupper">
+               <div class="introphoto">
+                  <h1>We get your product to the people that need it.</h1>
+                  <div class="introphotospace">
+                  </div>
+                  <img src="images/tubes.jpg"/>
+               </div>
+               <div class="introphotofoldright">
+               </div>
+            </div>
+            <div class="introphotolower">
+               <div class="introphotofoldleft">
+               </div>
+               <div class="introphoto">
+                  <img src="images/workers.jpg"/>
+                  <div class="introphotospace">
+                  </div>
+               </div>
+
+            </div>
          </div>
       </div>
       </div>
@@ -72,11 +101,11 @@
          <div class="productblocks">
 
             <div class="productblock">
-               <div class="producticon">
+               <a href="http://www.rectorseal.com" class="producticon">
                   <img src="images/producticons/rectorseal.svg"/>
-               </div>
+               </a>
                <div class="productinfo">
-                  <h2>Rectorseal</h2>
+                  <a href="http://www.rectorseal.com"><h2>Rectorseal</h2></a>
                   <h3>Houston, TX</h3>
                   <ul>
                      <li>Thread Sealants</li>
@@ -95,11 +124,11 @@
             </div>
 
             <div class="productblock">
-               <div class="producticon">
+               <a href="http://www.siouxchief.com" class="producticon">
                   <img src="images/producticons/siouxchief.svg"/>
-               </div>
+               </a>
                <div class="productinfo">
-                  <h2>Sioux Chief</h2>
+                  <a href="http://www.siouxchief.com"><h2>Sioux Chief</h2></a>
                   <h3>Peculiar, MO</h3>
                   <ul>
                      <li>Finish Line Drainage System</li>
@@ -112,11 +141,11 @@
             </div>
 
             <div class="productblock">
-               <div class="producticon">
+               <a href="http://www.libertpumps.com" class="producticon">
                   <img src="images/producticons/libertypumps.svg"/>
-               </div>
+               </a>
                <div class="productinfo">
-                  <h2>Liberty Pumps</h2>
+                  <a href="http://www.libertpumps.com"><h2>Liberty Pumps</h2></a>
                   <h3>Bergen, NY</h3>
                   <ul>
                      <li>Submersible Sewage, Effluent, and Sump Pumps</li>
@@ -129,11 +158,11 @@
             </div>
 
             <div class="productblock">
-               <div class="producticon">
+               <a href="http://www.simmonsmfg.com/" class="producticon">
                   <img src="images/producticons/simmons.svg"/>
-               </div>
+               </a>
                <div class="productinfo">
-                  <h2>Simmons Manufacturing</h2>
+                  <a href="http://www.simmonsmfg.com/"><h2>Simmons Manufacturing</h2></a>
                   <h3>McDonough, GA</h3>
                   <ul>
                      <li>Well Accessories</li>
@@ -145,22 +174,114 @@
             </div>
 
             <div class="productblock">
-               <div class="producticon">
+               <a href="http://www.northernpipe.com/" class="producticon">
                   <img src="images/producticons/northernpipe.svg"/>
-               </div>
+               </a>
                <div class="productinfo">
-                  <h2>Northern Pipe Products</h2>
+                  <a href="http://www.northernpipe.com/"><h2>Northern Pipe Products</h2></a>
                   <h3>Fargo, ND</h3>
                   <ul>
                      <li>PVC Pipe for Plumbing, Irrigation, &amp; Well Markets</li>
                   </ul>
                </div>
             </div>
-
          </div>
       </div>
 
+      <div class="employees" id="team">
+         <h1>Our Team</h1>
+         <div class="employeeblocks">
+
+            <div class="employeeblockleft">
+               <div class="employeeinner">
+                  <div class="employeeupper">
+                     <div class="employeeimg">
+                        <img src="../images/headshot.jpg"/>
+                     </div>
+                     <div class="employeeinfo">
+                        <h1>Jeff Tyler</h1>
+                        <h2>Outside Sales</h2>
+                        <h3>Email: <a href="mailto:jeffreyt@northlandreps.com">jeffreyt@northlandreps.com</a></h3>
+                     </div>
+                  </div>
+                  <div class="employeelower">
+                     <p class="employeebio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin ligula rutrum dui malesuada, nec fermentum erat posuere. Donec lectus turpis, iaculis id semper vel, auctor non massa. Quisque sed molestie velit. Fusce efficitur tortor sed nisi varius, ut rhoncus tortor varius. Morbi lobortis dui sed ultricies iaculis. Sed sit amet nunc ante. </p>
+                  </div>
+               </div>
+            </div>
+
+            <div class="employeeblockright">
+               <div class="employeeinner">
+                  <div class="employeeupper">
+                     <div class="employeeinfo">
+                        <h1>Kevin Barta</h1>
+                        <h2>Outside Sales</h2>
+                        <h3>Email: <a href="mailto:kevinb@northlandreps.com">kevinb@northlandreps.com</a></h3>
+                     </div>
+                     <div class="employeeimg">
+                        <img src="../images/headshot2.jpg"/>
+                     </div>
+                  </div>
+                  <div class="employeelower">
+                     <p class="employeebio">Mauris vulputate, dui et gravida faucibus, velit lectus pretium justo, a consectetur ante est quis neque. Nulla facilisi. Nullam non lectus risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam hendrerit pharetra nisl, vel congue est dictum ac. </p>
+                  </div>
+               </div>
+            </div>
+
+            <div class="employeeblockleft">
+               <div class="employeeinner">
+                  <div class="employeeupper">
+                     <div class="employeeimg">
+                        <img src="../images/headshot3.jpg"/>
+                     </div>
+                     <div class="employeeinfo">
+                        <h1>Cristina Zukowski</h1>
+                        <h2>Inside Sales</h2>
+                        <h3>Email: <a href="mailto:cristinaz@northlandreps.com">cristinaz@northlandreps.com</a></h3>
+                     </div>
+                  </div>
+                  <div class="employeelower">
+                     <p class="employeebio">Ut urna ante, dignissim eu suscipit eget, facilisis venenatis purus. Nam mi enim, tincidunt id commodo sit amet, dignissim a massa. Phasellus est felis, commodo non lorem mattis, finibus rhoncus nisl. </p>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+
+      </div>
+
+      <div class="contact" id="contact">
+         <h1>Contact</h1>
+         <div class="contactinner">
+            <div class="contactupper">
+               <div class="contactupperleft">
+                  <div><h2>Phone</h2><a href="tel:952-854-3905">&#40;952&#41; 854-3905</a></div>
+                  <div><h2>Fax</h2><a href="fax:952-854-5210">&#40;952&#41; 854-5210</a></div>
+                  <div><h2>Address</h2><a href="https://www.google.com/maps/place/500+3rd+St+SE,+Osseo,+MN+55369/@45.1122785,-93.397816,17z/data=!3m1!4b1!4m5!3m4!1s0x52b339cc63dfc2f3:0x3570656943a6eef5!8m2!3d45.1122747!4d-93.3956273">500 Third St. SE</br>Osseo, MN 55369</a></div>
+               </div>
+               <div class="contactupperright">
+                  <div id="map"></div>
+               </div>
+            </div>
+            <div class="contactlower">
+               <div class="contactlowerinner">
+                  <textarea name="message" form="contactform" placeholder="Send A Message"></textarea>
+                  <form action="" id="contactform">
+                    <input type="text" name="usrname" placeholder="Name">
+                    <input type="text" name="usremail" placeholder="Email">
+                    <input type="submit" value="Send">
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="copyright">&#169; <?php
+      date_default_timezone_set('America/Los_Angeles');
+      echo strftime("%Y"); ?> Northland Reps Inc.</div>
+
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
       <script type="text/javascript" src="js/customscripts.js"></script>
-   </body>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCixrYStmQEAiNMYB131hXajWX8WEYwP2Y&callback=initMap"async defer></script>
+  </body>
 </html>
--- wwwroot/js/customscripts.js
+++ wwwroot/js/customscripts.js
@@ -1,22 +1,58 @@
 $(document).ready(function() {
    var belowHeader = 0;
+   var windowSize;
+   var headerHeight;
+   var navbarHeight
+
+   if ($(window).width() > 992) {
+      windowSize = 3;
+   }
+   else if ($(window).width() <= 992) {
+      windowSize = 2;
+   }
+   $(window).resize(function() {
+      if ($(window).width() > 992) {
+         windowSize = 3;
+      }
+      else if ($(window).width() <= 992) {
+         windowSize = 2;
+      }
+   });
+
    $(window).scroll(function () {
-      var headerHeight = $('.header').height() + 14;
-      var navbarHeight = $('.navbar').height();
+      headerHeight = $('.header').height() + 14;
+      navbarHeight = $('.navbar').height();
       console.log($(window).scrollTop())
-      if ($(window).scrollTop() > headerHeight && belowHeader == 0) {
-         $('.navlogo').addClass('navexpand');
-         $('.navlogoimg').addClass('imgexpand');
-         $('.navbar').css({'fontSize': '1.6vw'});
+
+      if ($(window).scrollTop() > headerHeight) {
+         if (belowHeader == 0) {
+            $('.navlogo').addClass('navexpand');
+            $('.navlogoimg').addClass('imgexpand');
+
+            if (windowSize == 3) {
+               $('.navbar').css({'fontSize': '1.6vw'});
+            }
+            else if (windowSize == 2) {
+               $('.navbar').css({'fontSize': '1.8vw'});
+            }
+
+            $('.navbarfill').css({height: navbarHeight});
+            $('.navbarfill').show();
+         }
          $('.navbarwrap').css({position: "fixed", top:0});
-         $('.navbarfill').css({height: navbarHeight + 1});
-         $('.navbarfill').show();
          belowHeader = 1;
       }
       else if ($(window).scrollTop() <= headerHeight && belowHeader == 1) {
          $('.navlogo').removeClass('navexpand');
          $('.navlogoimg').removeClass('imgexpand');
-         $('.navbar').css({'fontSize': '1.2vw'});
+
+         if (windowSize == 3) {
+            $('.navbar').css({'fontSize': '1.2vw'});
+         }
+         else if (windowSize == 2) {
+            $('.navbar').css({'fontSize': '1.4vw'});
+         }
+
          $('.navbarwrap').css({position: "absolute", top:headerHeight});
          setTimeout(function(){
             $('.navbarfill').hide();
@@ -27,7 +63,20 @@ $(document).ready(function() {
   });
 });
 
-
+function initMap() {
+   // Create a map object and specify the DOM element for display.
+   var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.112347, lng: -93.395466},
+      scrollwheel: true,
+      zoom: 17
+   });
+   var image = '../images/icon.png';
+   var beachMarker = new google.maps.Marker({
+      position: {lat: 45.112347, lng: -93.395466},
+      map: map,
+      icon: image
+   });
+}
 
 function getElementY(query) {
   return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
@@ -68,6 +117,8 @@ function doScrolling(element, duration) {
 }
 
 // Apply event handlers. Example of firing the scrolling mechanism.
-document.getElementById('headeranchor').addEventListener('click', doScrolling.bind(null, '#header', 600))
-document.getElementById('aboutanchor').addEventListener('click', doScrolling.bind(null, '#intro', 600))
-document.getElementById('productsanchor').addEventListener('click', doScrolling.bind(null, '#products', 600))
+document.getElementById('headeranchor').addEventListener('click', doScrolling.bind(null, '#header', 700))
+document.getElementById('aboutanchor').addEventListener('click', doScrolling.bind(null, '#intro', 700))
+document.getElementById('productsanchor').addEventListener('click', doScrolling.bind(null, '#products', 700))
+document.getElementById('teamanchor').addEventListener('click', doScrolling.bind(null, '#team', 700))
+document.getElementById('contactanchor').addEventListener('click', doScrolling.bind(null, '#contact', 700))
